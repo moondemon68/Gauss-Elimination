39f9a07226a3e0ebb91fefc89fcd5ee60030add2
Morgen Sudyanto <moondemon68@gmail.com>
2019-09-11 21:21:02 +0700

    Add starting files

---
+++ MainProg.java
@@ -0,0 +1,7 @@
+class MainProg {
+
+    public static void main(String[] args) {
+        matriks M = new matriks();
+        
+    }
+}
---
+++ matriks.java
@@ -0,0 +1,13 @@
+class matriks {
+
+    int [][] Mat = new int[105][105];
+
+    matriks() {
+        for (int i=1;i<=100;i++) {
+            for (int j=1;j<=100;j++) {
+                this.Mat[i][j] = 0;
+            }
+        }
+    }
+    
+}
