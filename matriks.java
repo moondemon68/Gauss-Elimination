e228b4d53ea5cffb612d858b86d3a0b59d474714
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2019-09-13 18:59:30 +0700

    commit

--- matriks.java
+++ matriks.java
@@ -4,7 +4,7 @@ class matriks {
 
     matriks() {
         for (int i = 1; i <= 100; i++) {
-            for (int j = 1 ; j <= 100; j++) {
+            for (int j = 1; j <= 100; j++) {
                 this.Mat[i][j] = 0.0;
             }
         }
