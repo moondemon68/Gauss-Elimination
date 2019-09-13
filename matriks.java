8dd4b147fa628f0d5d72a09b4281a0015129884e
Muhammad Hasan <39514247+muhammadhasan01@users.noreply.github.com>
2019-09-13 18:57:38 +0700

    int -> double

--- matriks.java
+++ matriks.java
@@ -1,11 +1,11 @@
 class matriks {
 
-    int [][] Mat = new int[105][105];
+    Double [][] Mat = new Double[105][105];
 
     matriks() {
         for (int i = 1; i <= 100; i++) {
             for (int j = 1 ; j <= 100; j++) {
-                this.Mat[i][j] = 0;
+                this.Mat[i][j] = 0.0;
             }
         }
     }
