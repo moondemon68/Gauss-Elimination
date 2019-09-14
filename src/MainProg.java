bd7cd5e3d3b0c1202323c0f77263907c03fd250a
Morgen Sudyanto <moondemon68@gmail.com>
2019-09-14 18:20:19 +0700

    rapihin biar bisa kecompile ehe

---
+++ src/MainProg.java
@@ -0,0 +1,23 @@
+class MainProg {
+
+    /*
+    ----- MENU -----
+    1. Sistem Persamaan Linier
+    2. Determinan
+    3. Matriks balikan
+    4. Matriks kofaktor
+    5. Adjoin
+    6. Interpolasi Polinom
+    7. Keluar
+    ----- SUBMENU ----
+    1. Metode Eliminasi Gauss
+    2. Metode Eliminasi Gauss-Jordan
+    3. Metode Matriks balikan
+    4. Kaidah Crammer
+    */
+
+    public static void main(String[] args) {
+        matriks M = new matriks();
+        System.out.println("KECOMPILEGAN");
+    }
+}
---
+++ src/matriks.java
@@ -0,0 +1,13 @@
+public class matriks {
+
+    double [][] Mat = new double[105][105];
+
+    matriks() {
+        for (int i = 1; i <= 100; i++) {
+            for (int j = 1; j <= 100; j++) {
+                this.Mat[i][j] = 0.0;
+            }
+        }
+    }
+
+}
